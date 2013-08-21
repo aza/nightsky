81ab9948e2f67618fbf9fab1db16a1371bdce163
Aza Raskin <aza@luggage.local>
2013-08-21 02:29:34 -0700

    Playing around with shaders

---
+++ shaders/cloudFlare.c
@@ -0,0 +1,204 @@
+// Created by inigo quilez - iq/2013
+// License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+
+//#define FULL_PROCEDURAL
+
+
+#ifdef FULL_PROCEDURAL
+
+// hash based 3d value noise
+float hash( float n )
+{
+    return fract(sin(n)*3538.5453);
+}
+float noise( in vec3 x )
+{
+    vec3 p = floor(x);
+    vec3 f = fract(x);
+	
+    f = f*f*(3.0-2.0*f);
+    float n = p.x + p.y*57.0 + 113.0*p.z;
+    return mix(mix(mix( hash(n+  0.0), hash(n+  1.0),f.x),
+                   mix( hash(n+ 57.0), hash(n+ 58.0),f.x),f.y),
+               mix(mix( hash(n+113.0), hash(n+114.0),f.x),
+                   mix( hash(n+170.0), hash(n+171.0),f.x),f.y),f.z);
+}
+#else
+
+float noise(float t)
+{
+	return texture2D(iChannel0,vec2(t,.0)/iChannelResolution[0].xy).x;
+}
+
+
+float noise(vec2 t)
+{
+	return texture2D(iChannel0,t/iChannelResolution[0].xy).x;
+}
+
+vec3 lensflare(vec2 uv,vec2 pos)
+{
+	vec2 main = uv-pos;
+	vec2 uvd = uv*(length(uv));
+	
+	float ang = atan(main.x,main.y);
+	float dist=length(main); dist = pow(dist,.1);
+	float n = noise(vec2(ang*16.0,dist*32.0));
+	
+	float f0 = 1.0/(length(uv-pos)*16.0+1.0);
+	
+	f0 = f0+f0*(sin(noise((pos.x+pos.y)*2.2+ang*4.0+5.954)*16.0)*.1+dist*.1+.8);
+	
+	float f1 = max(0.01-pow(length(uv+1.2*pos),1.9),.0)*7.0;
+
+	float f2 = max(1.0/(1.0+32.0*pow(length(uvd+0.8*pos),2.0)),.0)*00.25;
+	float f22 = max(1.0/(1.0+32.0*pow(length(uvd+0.85*pos),2.0)),.0)*00.23;
+	float f23 = max(1.0/(1.0+32.0*pow(length(uvd+0.9*pos),2.0)),.0)*00.21;
+	
+	vec2 uvx = mix(uv,uvd,-0.5);
+	
+	float f4 = max(0.01-pow(length(uvx+0.4*pos),2.4),.0)*6.0;
+	float f42 = max(0.01-pow(length(uvx+0.45*pos),2.4),.0)*5.0;
+	float f43 = max(0.01-pow(length(uvx+0.5*pos),2.4),.0)*3.0;
+	
+	uvx = mix(uv,uvd,-.4);
+	
+	float f5 = max(0.01-pow(length(uvx+0.2*pos),5.5),.0)*2.0;
+	float f52 = max(0.01-pow(length(uvx+0.4*pos),5.5),.0)*2.0;
+	float f53 = max(0.01-pow(length(uvx+0.6*pos),5.5),.0)*2.0;
+	
+	uvx = mix(uv,uvd,-0.5);
+	
+	float f6 = max(0.01-pow(length(uvx-0.3*pos),1.6),.0)*6.0;
+	float f62 = max(0.01-pow(length(uvx-0.325*pos),1.6),.0)*3.0;
+	float f63 = max(0.01-pow(length(uvx-0.35*pos),1.6),.0)*5.0;
+	
+	vec3 c = vec3(.0);
+	
+	c.r+=f2+f4+f5+f6; c.g+=f22+f42+f52+f62; c.b+=f23+f43+f53+f63;
+	c = c*1.3 - vec3(length(uvd)*.05);
+	c+=vec3(f0);
+	
+	return c;
+}
+
+vec3 cc(vec3 color, float factor,float factor2) // color modifier
+{
+	float w = color.x+color.y+color.z;
+	return mix(color,vec3(w)*factor,w*factor2);
+}
+
+// LUT based 3d value noise
+float noise( in vec3 x )
+{
+    vec3 p = floor(x);
+    vec3 f = fract(x);
+	vec2 mo = -1.0 + 2.0*iMouse.xy / iResolution.xy;
+	f = f*f*(3.0-2.0*f);
+	
+	vec2 uv = (p.xy+vec2(37.0,17.0)*p.z) + f.xy + iGlobalTime/10.0;
+	vec2 rg = texture2D( iChannel0, (uv+ 0.5)/256.0, -100.0 ).yx;
+	return mix( rg.x, rg.y, f.z );
+}
+#endif
+
+vec4 map( in vec3 p )
+{
+	float d = 0.2 - p.y;
+
+	vec3 q = p - vec3(0.2,0.1,0.0)*iGlobalTime;
+	float f;
+    f  = 0.5000*noise( q ); q = q*2.02;
+    f += 0.2500*noise( q ); q = q*2.03;
+    f += 0.1250*noise( q ); q = q*2.01;
+    f += 0.0625*noise( q );
+
+	d += 3.0 * f;
+
+	d = clamp( d, 0.0, 1.0 );
+	
+	vec4 res = vec4( d );
+
+	res.xyz = mix( 1.2*vec3(1.0,0.95,0.8), vec3(0.8,0.6,0.5), res.x );
+	
+	return res;
+}
+
+
+vec3 sundir = vec3(-1.0,0.0,0.0);
+
+
+vec4 raymarch( in vec3 ro, in vec3 rd )
+{
+	vec4 sum = vec4(0, 0, 0, 0);
+
+	float t = 0.0;
+	for(int i=0; i<64; i++)
+	{
+		if( sum.a > 0.99 ) continue;
+
+		vec3 pos = ro + t*rd;
+		vec4 col = map( pos );
+		
+		#if 1
+		float dif =  clamp((col.w - map(pos+0.3*sundir).w)/0.6, 0.0, 1.0 );
+
+        vec3 lin = vec3(0.65,0.68,0.7)*1.35 + 0.45*vec3(0.7, 0.5, 0.3)*dif;
+		col.xyz *= lin;
+		#endif
+		
+		col.a *= 0.35;
+		col.rgb *= col.a;
+
+		sum = sum + col*(1.0 - sum.a);	
+
+        #if 0
+		t += 0.1;
+		#else
+		t += max(0.1,0.025*t);
+		#endif
+	}
+
+	sum.xyz /= (0.001+sum.w);
+
+	return clamp( sum, 0.0, 1.0 );
+}
+
+void main(void)
+{
+	vec2 q = gl_FragCoord.xy / iResolution.xy;
+    vec2 p = -1.0 + 2.0*q;
+    p.x *= iResolution.x/ iResolution.y;
+    vec2 mo = -1.0 + 2.0*iMouse.xy / iResolution.xy;
+    
+    // camera
+    vec3 ro = 4.0*normalize(vec3(cos(2.75-3.0*mo.x), 0.7+2.0*sin((mo.y+1.0)), 2.0*sin(2.75-3.0*mo.x)));
+	vec3 ta = vec3(0.0, 1.0, 0.0);
+    vec3 ww = normalize( ta - ro);
+    vec3 uu = normalize(cross( vec3(0.0,1.0,0.0), ww ));
+    vec3 vv = normalize(cross(ww,uu));
+    vec3 rd = normalize( p.x*uu + p.y*vv + 1.5*ww );
+
+	
+    vec4 res = raymarch( ro, rd );
+
+	float sun = clamp( dot(sundir,rd), 0.0, 1.0 );
+	vec3 col = vec3(0.6,0.71,0.75) - rd.y*0.2*vec3(1.0,0.5,1.0) + 0.15*0.5;
+	col += 0.2*vec3(1.0,.6,0.1)*pow( sun, 8.0 );
+	col *= 0.95;
+	col = mix( col, res.xyz, res.w );
+	col += 0.1*vec3(1.0,0.4,0.2)*pow( sun, 3.0 );
+	    
+    gl_FragColor = vec4( col, 1.0 );
+	
+	vec2 uv = gl_FragCoord.xy / iResolution.xy - 0.5;
+	uv.x *= iResolution.x/iResolution.y; //fix aspect ratio
+	vec3 mouse = vec3(iMouse.xy/iResolution.xy - 0.5,iMouse.z-.5);
+	mouse.x *= iResolution.x/iResolution.y; //fix aspect ratio
+
+	
+	vec3 color = vec3(1.4,1.2,1.0)*lensflare(uv,-mouse.xy);
+	color -= noise(gl_FragCoord.xy)*.015;
+	color = cc(color,.5,.1);
+	gl_FragColor = vec4(col*color/.7,1.0);
+}
